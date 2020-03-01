f325f5e2dc2ae8a00ad69bca5f805a4b719c555b
984580403hyxhj <stevenxue2001@gmail.com>
2020-03-01 17:01:34 -0500

    sfsf

--- main.c
+++ main.c
@@ -6,54 +6,16 @@
 #include "packing.h"
 
 
-#define COUNT 10
-
-void print2DUtil(node *root, int space) 
-{ 
-    // Base case 
-    if (root == NULL) 
-        return; 
-  
-    // Increase distance between levels 
-    space += COUNT; 
-  
-    // Process right child first 
-    print2DUtil(root->right, space); 
-  
-    // Print current node after space 
-    // count 
-    printf("\n"); 
-    for (int i = COUNT; i < space; i++) 
-        printf(" "); 
-    printf("%c, (%d, %d)(%d, %d)\n", root->value, root->xlength, root->ylength, root->xcor, root->ycor); 
-  
-    // Process left child 
-    print2DUtil(root->left, space); 
-} 
-  
-// Wrapper over print2DUtil() 
-void print2D(node *root) 
-{ 
-   // Pass initial space count as 0 
-   print2DUtil(root, 0); 
-} 
-
-
-
-
-
-
-
 int main(int argc, char** argv)
 {
     queue *head = buildlist(argv[1]);
 
     head = reversequeue(head);
-    queue *temp = head;
 
+    queue *temp = head;
     while(temp != NULL)
     {
-        printf("%c, (%d, %d)\n",temp->data->value, temp->data->xlength, temp->data->ylength);
+        printf("%c(%d,%d)\n",temp->data->value,temp->data->xlength,temp->data->ylength);
         temp = temp->next;
     }
 
@@ -64,8 +26,6 @@ int main(int argc, char** argv)
     output2(treehead, argv[3]);
 
     modifytree(treehead);
-
-    print2D(treehead);
 
     output3(treehead, argv[4]);
 
